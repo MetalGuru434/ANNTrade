a519f7ed92df77e4bde2be9d43bf454f5a3ad5a0
MetalGuru434 <mikhailov@web.de>
2025-07-09 22:10:30 +0200

    Update Basket.py

--- Basket.py
+++ Basket.py
@@ -6,3 +6,80 @@ gdown.download('https://storage.yandexcloud.net/aiueducation/Content/base/l10/ba
 import pandas as pd
 df = pd.read_csv('basketball.csv', encoding= 'cp1251', sep=';', header=0, index_col=0) # Загружаем базу
 df.head()
+
+#Извлекаем текстовые данные из колонки `info` таблицы, помещаем в переменную `data_text`. 
+#Выводим длину списка:
+
+data_text = df['info'].values #
+
+len(data_text) #
+
+#Задаем максимальное кол-во слов в словаре, помещаем в переменную все символы, 
+#которые хотим вычистить из текста.
+#Токенизируем текстовые данные:
+
+# Импортируем токенайзер
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+maxWordsCount = 5000
+
+sim_for_del='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n'
+
+tokenizer = Tokenizer (num_words=maxWordsCount,
+                       filters=sim_for_del,
+                       lower=True,
+                       split=' ',
+                       oov_token='unknown',
+                       char_level=False)
+
+tokenizer.fit_on_texts(data_text)
+
+# Переводим в Эмбеддинг пространство
+Sequences = tokenizer.texts_to_sequences(data_text)
+
+# Вариант  Bag of Words
+xBOW_text = tokenizer.sequences_to_matrix(Sequences)
+
+#Преобразуем данные в numpy, подготовим наборы для обучения
+# Библиотека работы с массивами
+import numpy as np
+
+xTrain = np.array(df[['Ком. 1','Ком. 2', 'Минута', 'Секунда','ftime']].astype('int'))
+yTrain = np.array(df['fcount'].astype('int'))
+
+print(xTrain.shape)
+print(yTrain.shape)
+print(xBOW_text.shape)
+
+
+# Функция по проверке ошибки
+
+def check_MAE_predictl_DubbleInput (model,
+                                    x_data,
+                                    x_data_text,
+                                    y_data_not_scaled,
+                                    plot=False):
+
+  mae = 0 # Инициализируем начальное значение ошибки
+  y_pred = (model.predict([x_data,x_data_text])).squeeze()
+
+  for n in range (0,len(x_data)):
+    mae += abs(y_data_not_scaled[n] - y_pred[n]) # Увеличиваем значение ошибки для текущего элемента
+  mae /= len(x_data) # Считаем среднее значение
+  print('Среднаяя абслолютная ошибка {:.3f} очков это {:.3f}% от общей выборки в {} игры'.format(mae, (mae/y_data_not_scaled.mean(axis=0))*100,len(x_data)))
+
+  if plot:
+     plt.scatter(y_data_not_scaled, y_pred)
+     plt.xlabel('Правильные значение')
+     plt.ylabel('Предсказания')
+     plt.axis('equal')
+     plt.xlim(plt.xlim())
+     plt.ylim(plt.ylim())
+     plt.plot([0, 250], [0, 250])
+     plt.show()
+
+# ваше решение
+
+
+
+
