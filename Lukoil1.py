5bcaf0b9f9c9d4fad88b75a6c79c50f3615def40
MetalGuru434 <mikhailov@web.de>
2025-07-11 10:37:53 +0200

    Update Lukoil1.py

--- Lukoil1.py
+++ Lukoil1.py
@@ -1,1 +1,113 @@
+# Работа с массивами
+import numpy as np
+
+# Работа с таблицами
+import pandas as pd
+
+# Классы-конструкторы моделей нейронных сетей
+from tensorflow.keras.models import Sequential, Model
+
+# Основные слои
+from tensorflow.keras.layers import concatenate, Input, Dense, Dropout, BatchNormalization, Flatten, Conv1D, LSTM, MaxPooling1D
+
+# Оптимизаторы
+from tensorflow.keras.optimizers import Adam
+
+# Генератор выборки временных рядов
+from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
+
+# Нормировщики
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+# Коллбэк
+from tensorflow.keras.callbacks import LearningRateScheduler
+
+# Загрузка датасетов из облака google
+import gdown
+
+# Отрисовка графиков
+import matplotlib.pyplot as plt
+
+# Отрисовка графики в ячейке colab
+#%matplotlib inline
+
+# Отключение предупреждений
+import warnings
+warnings.filterwarnings('ignore')
+
+# Назначение размера и стиля графиков по умолчанию
+
+from pylab import rcParams
+plt.style.use('ggplot')
+rcParams['figure.figsize'] = (14, 7)
+
+# Загрузка датасетов из облака
+
+gdown.download('https://storage.yandexcloud.net/aiueducation/Content/base/l11/16_17.csv', None, quiet=True)
+gdown.download('https://storage.yandexcloud.net/aiueducation/Content/base/l11/18_19.csv', None, quiet=True)
+
+
+# Чтение данных в таблицы с удалением ненужных столбцов
+
+data16_17 = pd.read_csv('16_17.csv', sep=';').drop(columns=['DATE', 'TIME'])
+data18_19 = pd.read_csv('18_19.csv', sep=';').drop(columns=['DATE', 'TIME'])
+
+# Просмотр размерностей получившихся таблиц
+
+print(data16_17.shape)
+print(data18_19.shape)
+
+
+# Создание общего набора данных из двух датасетов
+
+data = pd.concat([data16_17,data18_19])   # Объединение датасетов
+data = data.reset_index(drop = True)      # Обнуление индексов
+
+# Проверка формы данных
+print(data.shape)
+
+
+# Получение названий столбцов
+
+col = data.columns
+print(col)
+
+
+# Задание циклов для столбцов таким образом, чтобы происходил перебор всех возможных пар:
+# Перебор индексов массива с именами столбцов 'OPEN', 'MAX', 'MIN', 'CLOSE', 'VOLUME',
+# получая пары 'OPEN' и 'MAX', 'OPEN' и 'MIN', 'OPEN' и 'CLOSE' ..., 'MAX' и 'MIN', 'MAX' и 'CLOSE' и т.д
+
+for i in range(col.shape[0]): # Для всех пар
+    for j in range(i + 1 , col.shape[0]): # Расчет
+        data[col[i] + '-' + col[j]] = data[col[i]] - data[col[j]] # Разности
+        data['|' + col[i] + '-' + col[j] + '|'] = abs(data[col[i]] - data[col[j]]) # Модулей разностей
+        data[col[i] + '*' + col[j]] = data[col[i]] * data[col[j]] # Произведения
+
+# Для каждого столбца 'OPEN', 'MAX', 'MIN', 'CLOSE', 'VOLUME' расчет:
+for i in col:
+    # Обратные значения. 1e-3 в формуле нужно, чтобы случайно не разделить на 0
+    data['Обратный ' + i] = 1 / (data[i] + 1e-3)
+    # Создание пустого столбца
+    data['Производная от ' + i] = np.nan
+    # При помощи срезов расчет первых производных, .reset_index(drop=True) нужен для корректных расчетов
+    data['Производная от ' + i][1:] = data[i][1:].reset_index(drop=True) - data[i][:-1].reset_index(drop=True)
+    # Создание пустого столбца
+    data['Вторая производная от ' + i] = np.nan
+    # При помощи срезов расчет вторых производных
+    data['Вторая производная от ' + i][2:] = data[i][2:].reset_index(drop=True) - 2 * data[i][1:-1].reset_index(drop=True) + data[i][:-2].reset_index(drop=True)
+
+
+# Просмотр результатов
+data
+
+
+# Использование всех столбцов, кроме первых двух
+data = np.array(data.iloc[2:])
+
+# Перевод в numpy
+data = np.array(data)
+
+# Переменная, для использования одной и той же архитектуры под разные матрицы
+columnsamount = data.shape[1]
+
 
